588b6ff75e6d92fe0ad4a64824eda51dafe36bd7
Wildan Muttaqien <77467476+wildanMutt@users.noreply.github.com>
2024-11-30 11:05:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Toggle Class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika menu diklik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+//Klik diluar sidebar untuk menghilangkan nav
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
